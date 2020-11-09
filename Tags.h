5f5560899bfa2feb37e52f3c31f258177b1a5f6c
Eduardo Kussler <eduardoeugeniok@gmail.com>
2020-11-09 19:59:05 -0300

    Criada classe pra lidar com o arquivo de TAG

---
+++ Tags.h
@@ -0,0 +1,115 @@
+// Classe que irá manejar a tabela hash com as tags dos filmes
+// A tabela hash armazenará:
+// A tag - chave
+// Um vetor com os ids dos filmes associados a essa tag
+
+#include <math.h>
+#include <string>
+#include <iostream>
+#include "csv.h"
+
+
+using namespace std;
+
+typedef struct tag {
+  int movieId = 0;
+  string tag = "";
+}TAG;
+
+
+// Estrutura que guarda os dados necessários para manipulação da tabela Hash
+typedef struct DadosTabelaTag {
+  string tag = "";
+  int chave = -1;
+  // ids
+  vector<int> movieIds;
+  bool usado = false; //Se já foi usado alguma vez
+  bool ocupado = false; // Se está sendo usado no momento
+} DADOSTABELATAG;
+
+// Estrutura para o retorno da função que calcula o hash
+typedef struct posechave {
+  long unsigned int chave;
+  int pos;
+}POSECHAVE;
+
+
+class TagsFilme {
+  // São 27,278 ids, então uma tabela com esse valor de M deve manter um desempenho bom,
+  // ainda mais usando double hashing
+  static const int M = 550129;
+  static const int segundoPrimo = 182659;
+private:
+  DADOSTABELATAG tabela[M];
+  // Calcula o inteiro correspondente a string de tag
+  long unsigned int calcChave(string nome) {
+    unsigned int chave = 0;
+    int a = 37;
+    for(int i = 0; i < nome.length(); i++){
+      // Atribui um peso a cada caractere do nome
+      chave += (int)nome[i] * (int)pow(a, i);
+    }
+    return chave;
+  }
+  // Retorna uma estrutura com:
+  // pos, a posição na tabela
+  // chave, a chave calculada a partir da string tag     
+  POSECHAVE hash(string tag) {
+    int i = 0;
+    long unsigned int chave = calcChave(tag);
+    int pos = chave % M;
+    // Segundo hash em caso de colisão
+    while(tabela[pos].ocupado && tabela[pos].chave != chave) {
+      i++;
+      pos = (chave + (i *(1 + (chave % segundoPrimo)))) % M;
+      //printf("POSIÇÃO: %d", pos);
+      //printf("pos: %d", pos);
+    }
+    POSECHAVE posEchave;
+    posEchave.pos = pos;
+    posEchave.chave = chave;
+    return posEchave;
+  }
+public:
+  TagsFilme() = default;
+  // Insere pode receber dados de ratings.csv ou de movie.csv, e cada tem um comportamento
+  // diferente para cada um
+
+
+  // Insere recebendo um rating, apenas atualiza a média e o total de avaliações e movieId
+  void insere(TAG tag) {
+    POSECHAVE posEchave = hash(tag.tag);
+    int pos = posEchave.pos;
+    unsigned int chave = posEchave.chave;
+    tabela[pos].chave = chave;
+    tabela[pos].tag = tag.tag;
+    tabela[pos].movieIds.push_back(tag.movieId);
+    tabela[pos].ocupado = true;
+    tabela[pos].usado = true;
+  }
+
+
+  // Busca os dados do filme com tal id,
+  // Retorna um vector com os ids dos filmes com aquela tag;
+  // Retorna um vector com -1 se não encontrou; 
+  vector<int> busca(string tagBusca) {
+    int pos = 0;
+    POSECHAVE posEchave = hash(tagBusca);
+    long unsigned int chave = posEchave.chave;
+    int i = 0;
+    // Variavel temporária para formar o retorno da função
+    do {
+      pos = (chave + (i *(1 + (chave % segundoPrimo)))) % M;
+      if(tabela[pos].ocupado && tabela[pos].chave == chave) {
+        return tabela[pos].movieIds;
+      } else {
+        i++;
+      }
+    } while(tabela[pos].usado);
+    // Não encontrou filme com esse id  
+    vector<int> tmp;
+    tmp.push_back(-1);
+    return tmp;
+  }
+};
+
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include "csv.h"
 #include "TabelaHash.h"
+#include "Tags.h"
 
 using namespace io;
 using namespace std;
@@ -13,6 +14,7 @@ int main() {
   string lixo;
   DADOS busca;
   TabelaHash* hashtable = new TabelaHash();
+  TagsFilme* tabelaTags = new TagsFilme();
   CSVReader<4, trim_chars<' '>, double_quote_escape<',','\"'>> minirating ("./Dados_clean/rating.csv");
   minirating.read_header(ignore_no_column,"userId","movieId","rating","timestamp");
   while(minirating.read_row(avaliacao.userId, avaliacao.movieId,avaliacao.avaliacao, lixo)) {
@@ -26,8 +28,14 @@ int main() {
    //printf("movieID: %d\n title: %s, genres: %s", filme.movieId, filme.titulo.c_str(), filme.generos.c_str());
   }
  
-  busca = hashtable->busca(260);
-  cout << busca.titulo << endl;
-
+  CSVReader<4, trim_chars<' '>, double_quote_escape<',','\"'>> tags ("./Dados_clean/tag_clean.csv");
+  tags.read_header(ignore_no_column,"userId","movieId","tag","timestamp");
+  TAG tagLida;
+  int user;
+  while(tags.read_row(user, tagLida.movieId, tagLida.tag, lixo)) {
+    tabelaTags->insere(tagLida);
+  }
+  // Resultado esperado = 8641
+  cout << tabelaTags->busca("Steve Carell")[0] << endl;
   return 0;
 }
